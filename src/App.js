d9866baa8f47ad95381aebde3d111400e5c70cad
Hugo <HF77554@gmail.com>
2022-01-12 10:51:24 -0500

    added Stack at App.jsx with header and Button element

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Container>
+      <Stack direction='horizontal' gap='2' className='mb-4'>
+        <h1 className='me-auto'>Budget</h1>
+        <Button variatn='primary'>Add Budget</Button> 
+      </Stack>
+    </Container>
   );
 }
 
