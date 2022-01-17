1c5a43637a5d0a1db00d6083e7e41550af1d0948
Hugo <HF77554@gmail.com>
2022-01-17 13:13:04 -0500

    created a TotalPieCharDisplay Component

--- src/components/graphs/GraphDisplay.jsx
+++ src/components/graphs/GraphDisplay.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './graph.css'
 
 import PieChartCarousel from './PieChartCarousel'
+import TotalPieChart from './TotalPieChart'
 
 const GraphDisplay = () => {
     return (
         <div className='custom_graph_container my-4 rounded bg bg-light border border-5'>
+            <TotalPieChart />
             <PieChartCarousel />
-            Other
         </div>
     )
 }
---
+++ src/components/graphs/TotalPieChart.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './graph.css'
+
+import { useBudgets } from '../../contexts/BudgetContext';
+
+const TotalPieChart = () => {
+    
+    const { budgets, getBudgetExpenses } = useBudgets()
+
+    console.log(budgets)
+
+    //colors from which to chart colors are chosen from (limited, must expand)
+    const colors = ['#0275d8','#f0ad4e','#5cb85c','#5bc0de','#d9534f','#292b2c','#0275d8','#f0ad4e','#5cb85c','#5bc0de','#d9534f','#292b2c']
+
+    return (
+        <div className='custom_carousel_div'>
+            Test
+        </div>
+    )
+}
+
+export default TotalPieChart
+
+/*
+<div className='custom_carousel_div'>
+            {budgets.map((budget, int) => {
+
+                const amount = getBudgetExpenses(budget.id).reduce((total,expense) => total + expense.amount, 0)
+                const remainingBudget = budget.max-amount
+
+                const currentData = getBudgetExpenses(budget.id).map((expense,ind) => 
+                    {return (
+                        { 
+                            title:expense.description.toUpperCase() , 
+                            value: percentageValue( {amount:expense.amount, total:budget.max}) , 
+                            color:colors[ind]})
+                        })
+
+                //updates data to pass to pie chart, if within budget creates a new item to add as "unused" in which to put the remaining percetange
+                const data = (remainingBudget > 0) ? 
+                    [...currentData, 
+                        { 
+                            title:'UNUSED' , 
+                            value: percentageValue( {amount:remainingBudget, total:budget.max}), 
+                            color:'#899290'
+                        }] 
+                    : [...currentData]
+
+                return(
+                    <div key={`${budget.id}_${int}`} className='custom_carousel_item'>
+                        <h2 className='text-center'>{budget.name.toUpperCase()}</h2>
+                        <PieChartDisplay 
+                            data={data} 
+                            radius={40}
+                        />
+                    </div>
+                )})
+            }
+        </div>
+        */
